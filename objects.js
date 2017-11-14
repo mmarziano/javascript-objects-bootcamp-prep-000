7eb25d0c39cdb945cf6e4e33b203d852d25f1621
Monica Marziano <mmarziano1@gmail.com>
2017-11-14 10:19:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({artist: 'happybirthday'});
 
-function updatePlaylist ({}, 'Phil Ochs', "Here's to the State of Mississippi"){
-  return playlist
+function updatePlaylist () {
+  return object.assign({}, 'Phil Ochs', "Here's to the State of Mississippi")
 }
