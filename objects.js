31cf2e6a2e266d521751fd79f0798abc560cf305
Monica Marziano <mmarziano1@gmail.com>
2017-11-14 09:50:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,3 @@
-var meals {
-  breakfast: 'oatmeal';
-  lunch: 'burrito';
-  dinner: 'steak';
-}
-
-meals.snack = 'yogurt';
-
-console.log(meals.snack)
-
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value;
-  return obj
+var playlist = {
+  artistName = "song";
 }
